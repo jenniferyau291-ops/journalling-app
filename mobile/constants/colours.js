341b330391dcde3e530a8641cc1400f864dd0581
Jennifer-97-Y <yaulokwan@hotmail.co.uk>
2026-03-08 17:08:24 +0000

    Add mobile folder with login log out and register done

---
+++ mobile/constants/colours.js
@@ -0,0 +1,22 @@
+const COLOURS = {
+  primary: "#9C27B0",
+
+  textPrimary: "#4A148C",
+  textSecondary: "#7B1FA2",
+  textDark: "#311B92",
+  placeholderText: "#9E9E9E",
+
+  background: "#F3E5F5",
+  cardBackground: "#F8BFF8",
+  inputBackground: "#F3E5F5",
+
+  border: "#E1BEE7",
+  outline: "#D1C4E9",
+
+  error: "#D32F2F",     // Material Red 700
+  success: "#388E3C",   // Material Green 700
+
+  white: "#FFFFFF",
+  black: "#000000",
+};
+export default COLOURS;
